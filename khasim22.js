4505460fb21afeae58816d3bb4f175193c4ac4ec
Nasima <snasima1310@gmail.com>
2025-05-11 09:59:53 +0530

    Initial commit: Added Calculator project

---
+++ khasim22.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WelcomeMessage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>WELCOME TO REACT JS</h1>
+      </div>
+    );
+  }
+}
+
+export default WelcomeMessage;
